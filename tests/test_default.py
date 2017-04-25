f222bffac9069064d7b592e8900486d73246dda6
Deepak Kothandan <deepak.kothandan@valmano.de>
2017-04-25 10:16:17 +0200

    added testinfra tests

--- tests/test_default.py
+++ tests/test_default.py
@@ -17,3 +17,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ])
 def test_varnish_binaries_exists(host, name):
     assert host.exists(name) is True
+
+
+def test_varnish_is_enabled(host):
+    assert host.service('varnish').is_running is True
+    assert host.service('varnish').is_enabled is True
